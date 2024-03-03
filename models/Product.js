01c1b8aadf8903327a7dbc22cf7ecad50a2bfe26
Mel Mel Mom <melmelmomof4@gmail.com>
2024-03-02 21:03:03 -0500

    everything set up and port listening

--- models/Product.js
+++ models/Product.js
@@ -1,7 +1,7 @@
 // import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
-const sequelize = require('../../../config/connection');
+const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -21,11 +21,10 @@ Product.init(
       allowNull: false
     },
     price:{
-      type: DataTypes.DECIMAL(10,2),
+      type: DataTypes.DECIMAL,
       allowNull: false,
-      defaultValue: 10,
       validate: {
-        isNumeric: true
+        isDecimal: true
       }
     },
     stock: {
