52561d51ecc928b87a248607bc523a1c785ede47
Mel Mel Mom <melmelmomof4@gmail.com>
2024-03-06 23:16:39 -0500

    category POST route working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -39,7 +39,7 @@ router.post('/', async (req, res) => {
   // create a new category
   try {
     const locationData = await Category.create({
-      category_id: req.body.category_id,
+      category_name: req.body.category_name,
     });
     res.status(200).json(locationData);
   } catch (err) {
